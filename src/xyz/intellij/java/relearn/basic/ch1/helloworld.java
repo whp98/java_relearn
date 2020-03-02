13eabb8267f9353cc0bedcbd6bf1dad185686a86
whp <whp98@outlook.com>
2020-03-02 14:17:02 +0800

    init

---
+++ src/xyz/intellij/java/relearn/basic/ch1/helloworld.java
@@ -0,0 +1,11 @@
+package xyz.intellij.java.relearn.basic.ch1;
+/**
+ *  HelloWorld.java
+ *  作者：王海鹏
+ *  本程序在控制台中输出 hello world！
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
